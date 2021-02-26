e19c70244afd69894ebc25cf6e903f68af3b6afa
Artem_Savin <artemsavin2305@rambler.ru>
2021-02-27 00:00:30 +0700

    Add function Get_Paramert + removed magic constant + removed transliteration

--- geometry.c
+++ geometry.c
@@ -18,20 +18,36 @@ int Convert_String(char* str, int par)
     if (r == 0 || strlen(nstr) > 0) {
         return -1;
     }
-
     return r;
 }
 
-void Correct_Writing(const char* str, int max_symb, char* otvet)
+int Get_Parametr(const char* str, int i, char* sx)
+{
+    int j = 0;
+    char s[10] = "";
+    while ((str[i] != ' ') && (str[i] != ',') && (str[i] != ')')) {
+        s[j] = str[i];
+        i++;
+        j++;
+        if (j > 30) {
+            return -1;
+        }
+    }
+    s[j] = '\0';
+    strcpy(sx, s);
+    return i;
+}
+
+void Correct_Writing(const char* str, int max_symb, char* Answer)
 {
     char check_str[] = "circle";
-    int i, j;
+    int i;
     char sx[10] = "", sy[10] = "", sr[10] = "";
     double x, y, r;
     printf("\n\n");
 
-    if (strncmp(str, check_str, 6) != 0) {
-        sprintf(otvet,
+    if (strncmp(str, check_str, strlen(check_str)) != 0) {
+        sprintf(Answer,
                 "Mistake! Invalid input\n \"%s\" Not found \nDid you mean "
                 "\"circle\"?\n",
                 str);
@@ -40,78 +56,56 @@ void Correct_Writing(const char* str, int max_symb, char* otvet)
     i = 6;
 
     i = Skip_Space(str, i);
-
     if (str[i] != '(') {
-        sprintf(otvet, "Mistake! Invalid input\nNot found '('\n");
+        sprintf(Answer, "Mistake! Invalid input\nNot found '('\n");
         return;
     }
     i++;
 
     i = Skip_Space(str, i);
-
-    j = 0;
-    while (str[i] != ' ') {
-        sx[j] = str[i];
-        i++;
-        j++;
-        if (j > 30) {
-            sprintf(otvet,
-                    "Mistake! Invalid input\nNot found parametr function\n");
-            return;
-        }
+    i = Get_Parametr(str, i, sx);
+    if (i == -1) {
+        sprintf(Answer,
+                "Mistake! Invalid input\nNot found parametr X function\n");
+        return;
     }
-    sx[j] = '\0';
 
     i = Skip_Space(str, i);
-
-    j = 0;
-    while ((str[i] != ',') && (str[i] != ' ')) {
-        sy[j] = str[i];
-        i++;
-        j++;
-        if (j > 30) {
-            sprintf(otvet,
-                    "Mistake! Invalid input\nNot found parametr function\n");
-            return;
-        }
+    i = Get_Parametr(str, i, sy);
+    if (i == -1) {
+        sprintf(Answer,
+                "Mistake! Invalid input\nNot found parametr Y function\n");
+        return;
     }
-    sy[j] = '\0';
 
     if (str[i] != ',') {
-        sprintf(otvet, "Mistake! Invalid input\nNot found ','\n");
+        sprintf(Answer, "Mistake! Invalid input\nNot found ','\n");
         return;
     }
     i++;
 
     i = Skip_Space(str, i);
-
-    j = 0;
-    while ((str[i] != ')') && (str[i] != ' ')) {
-        sr[j] = str[i];
-        i++;
-        j++;
-        if (j > 30) {
-            sprintf(otvet,
-                    "Mistake! Invalid input\nNot found parametr function\n");
-            return;
-        }
+    i = Get_Parametr(str, i, sr);
+    if (i == -1) {
+        sprintf(Answer,
+                "Mistake! Invalid input\nNot found parametr R function\n");
+        return;
     }
-    sr[j] = '\0';
 
     if (str[i] != ')') {
-        sprintf(otvet, "Mistake! Invalid input\nNot found ')'\n");
+        sprintf(Answer, "Mistake! Invalid input\nNot found ')'\n");
         return;
     }
 
     if (i >= strlen(str)) {
-        sprintf(otvet, "Mistake! Invalid input\nNot found ')'\n");
+        sprintf(Answer, "Mistake! Invalid input\nNot found ')'\n");
         return;
     }
     i++;
 
     i = Skip_Space(str, i);
     if ((i + 1) < strlen(str)) {
-        sprintf(otvet, "Mistake! Invalid input\nUnexpected token\n");
+        sprintf(Answer, "Mistake! Invalid input\nUnexpected token\n");
         return;
     }
 
@@ -120,24 +114,24 @@ void Correct_Writing(const char* str, int max_symb, char* otvet)
     r = Convert_String(sr, 3);
 
     if (x == -1) {
-        sprintf(otvet, "Mistake! Invalid input\nParametr X\n");
+        sprintf(Answer, "Mistake! Invalid input\nParametr X\n");
         return;
     }
     if (y == -1) {
-        sprintf(otvet, "Mistake! Invalid input\nParametr Y\n");
+        sprintf(Answer, "Mistake! Invalid input\nParametr Y\n");
         return;
     }
     if (r == -1) {
-        sprintf(otvet, "Mistake! Invalid input\nParametr R\n");
+        sprintf(Answer, "Mistake! Invalid input\nParametr R\n");
         return;
     }
 }
 
 int main()
 {
-    int max_symb = 40;
+    const int max_symb = 40;
     FILE* input;
-    char otvet[N];
+    char Answer[N];
     input = fopen("input.txt", "r");
     if (input == NULL) {
         printf("Error open file\n");
@@ -147,8 +141,8 @@ int main()
     fgets(source_str, max_symb, input);
 
     fputs(source_str, stdout);
-    Correct_Writing(source_str, max_symb, otvet);
-    printf("%s\n", otvet);
+    Correct_Writing(source_str, max_symb, Answer);
+    printf("%s\n", Answer);
 
     return 0;
 }
