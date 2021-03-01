cdbcb1974446f6e93447612b30f075892f1d7a60
Artem_Savin <artemsavin2305@rambler.ru>
2021-03-02 01:02:22 +0700

    Removed unnecessary function arguments + made changes to the function Correct_Writting

--- geometry.c
+++ geometry.c
@@ -10,7 +10,7 @@ int Skip_Space(const char* str, int i)
     return i;
 }
 
-int Convert_String(char* str, int par)
+int Convert_String(char* str)
 {
     char* nstr;
     nstr = str;
@@ -38,16 +38,17 @@ int Get_Parametr(const char* str, int i, char* sx)
     return i;
 }
 
-void Correct_Writing(const char* str, int max_symb, char* Answer)
+void Correct_Writing(const char* str, char* result)
 {
     char check_str[] = "circle";
-    int i;
+    size_t i;
+    size_t j = -1;
     char sx[10] = "", sy[10] = "", sr[10] = "";
     double x, y, r;
     printf("\n\n");
 
     if (strncmp(str, check_str, strlen(check_str)) != 0) {
-        sprintf(Answer,
+        sprintf(result,
                 "Mistake! Invalid input\n \"%s\" Not found \nDid you mean "
                 "\"circle\"?\n",
                 str);
@@ -57,72 +58,72 @@ void Correct_Writing(const char* str, int max_symb, char* Answer)
 
     i = Skip_Space(str, i);
     if (str[i] != '(') {
-        sprintf(Answer, "Mistake! Invalid input\nNot found '('\n");
+        sprintf(result, "Mistake! Invalid input\nNot found '('\n");
         return;
     }
     i++;
 
     i = Skip_Space(str, i);
     i = Get_Parametr(str, i, sx);
-    if (i == -1) {
-        sprintf(Answer,
+    if (i == j) {
+        sprintf(result,
                 "Mistake! Invalid input\nNot found parametr X function\n");
         return;
     }
 
     i = Skip_Space(str, i);
     i = Get_Parametr(str, i, sy);
-    if (i == -1) {
-        sprintf(Answer,
+    if (i == j) {
+        sprintf(result,
                 "Mistake! Invalid input\nNot found parametr Y function\n");
         return;
     }
 
     if (str[i] != ',') {
-        sprintf(Answer, "Mistake! Invalid input\nNot found ','\n");
+        sprintf(result, "Mistake! Invalid input\nNot found ','\n");
         return;
     }
     i++;
 
     i = Skip_Space(str, i);
     i = Get_Parametr(str, i, sr);
-    if (i == -1) {
-        sprintf(Answer,
+    if (i == j) {
+        sprintf(result,
                 "Mistake! Invalid input\nNot found parametr R function\n");
         return;
     }
 
     if (str[i] != ')') {
-        sprintf(Answer, "Mistake! Invalid input\nNot found ')'\n");
+        sprintf(result, "Mistake! Invalid input\nNot found ')'\n");
         return;
     }
 
     if (i >= strlen(str)) {
-        sprintf(Answer, "Mistake! Invalid input\nNot found ')'\n");
+        sprintf(result, "Mistake! Invalid input\nNot found ')'\n");
         return;
     }
     i++;
 
     i = Skip_Space(str, i);
     if ((i + 1) < strlen(str)) {
-        sprintf(Answer, "Mistake! Invalid input\nUnexpected token\n");
+        sprintf(result, "Mistake! Invalid input\nUnexpected token\n");
         return;
     }
 
-    x = Convert_String(sx, 1);
-    y = Convert_String(sy, 2);
-    r = Convert_String(sr, 3);
+    x = Convert_String(sx);
+    y = Convert_String(sy);
+    r = Convert_String(sr);
 
-    if (x == -1) {
-        sprintf(Answer, "Mistake! Invalid input\nParametr X\n");
+    if (x == j) {
+        sprintf(result, "Mistake! Invalid input\nParametr X\n");
         return;
     }
-    if (y == -1) {
-        sprintf(Answer, "Mistake! Invalid input\nParametr Y\n");
+    if (y == j) {
+        sprintf(result, "Mistake! Invalid input\nParametr Y\n");
         return;
     }
     if (r == -1) {
-        sprintf(Answer, "Mistake! Invalid input\nParametr R\n");
+        sprintf(result, "Mistake! Invalid input\nParametr R\n");
         return;
     }
 }
@@ -131,7 +132,7 @@ int main()
 {
     const int max_symb = 40;
     FILE* input;
-    char Answer[N];
+    char result[N];
     input = fopen("input.txt", "r");
     if (input == NULL) {
         printf("Error open file\n");
@@ -141,8 +142,8 @@ int main()
     fgets(source_str, max_symb, input);
 
     fputs(source_str, stdout);
-    Correct_Writing(source_str, max_symb, Answer);
-    printf("%s\n", Answer);
+    Correct_Writing(source_str, result);
+    printf("%s\n", result);
 
     return 0;
 }
