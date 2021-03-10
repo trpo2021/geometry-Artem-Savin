c0ccffcdda55fc475c80afb8ebb7d8f0b7e45afa
Artem_Savin <artemsavin2305@rambler.ru>
2021-03-10 18:04:39 +0700

    The application developed in Lab 1 is divided into two modules: a static library and an application

---
+++ src/libgeometry/geometry.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <stdio.h>
+
+int Skip_Space(const char* str, int i);
+
+int Convert_String(char* str);
+
+int Get_Parametr(const char* str, int i, char* sx);
+
+void Correct_Writing(const char* str, char* result);
