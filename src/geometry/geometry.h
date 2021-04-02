88745fa2bf3ba3854b89891f6683eee2682ff2b1
Artem Savin <artemsavin2305@rambler.ru>
2021-04-03 00:35:59 +0700

    Minor changes

--- src/geometry/geometry.h
+++ src/geometry/geometry.h
@@ -1,8 +1,6 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-// #include <stdio.h>
-
 struct TCircle
 {
         double x, y, r;
@@ -13,4 +11,4 @@ void Correct_Writing(const char *str,  struct TCircle *c, char *result);
 void Calculate_area_and_perimeter( struct TCircle *c );
 
 
-#endif // GEOMETRY_H
+#endif
