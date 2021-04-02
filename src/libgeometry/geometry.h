03d8fbb9f8aa7525efe46e8e51ad37bba7f66a80
Artem_Savin <artemsavin2305@rambler.ru>
2021-04-02 17:53:41 +0700

    Structure name changed

--- src/libgeometry/geometry.h
+++ src/libgeometry/geometry.h
@@ -6,14 +6,12 @@
 typedef struct {
     double x, y, r;
     double p, s;
-
     void Calculate_area_and_perimeter()
     {
         const double pi = 3.141592;
         double p = 2 * pi * r;
         double s = pi * r * r;
         printf("perimetr=%f\narea=%f\n", p, s);
-        return;
     }
 } TCircle;
 
