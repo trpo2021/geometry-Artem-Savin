e426f68e427bcc2ffe2424ca47b7cee58696cd58
Artem_Savin <artemsavin2305@rambler.ru>
2021-04-03 12:03:05 +0700

    The code is formatted

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -1,21 +1,20 @@
 #include "geometry.h"
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-double sqr( double v )
+double sqr(double v)
 {
-    return v*v;    
+    return v * v;
 }
 
 int main()
 {
     const int max_symb = 40;
-    FILE *input;
+    FILE* input;
     char result[150];
     input = fopen("input.txt", "r");
-    if (input == NULL)
-    {
+    if (input == NULL) {
         printf("Error open file\n");
         return 1;
     }
@@ -25,8 +24,7 @@ int main()
     int cnt = 0;
     struct TCircle *c, *c2;
 
-    while (!feof(input))
-    {
+    while (!feof(input)) {
         source_str[0] = 0;
 
         fgets(source_str, sizeof(source_str), input);
@@ -35,14 +33,14 @@ int main()
 
         fputs(source_str, stdout);
 
-        c = (struct TCircle*) malloc(  100 );
+        c = (struct TCircle*)malloc(100);
 
         Correct_Writing(source_str, c, result);
 
         if (result[0] != 0)
             printf("%s\n", result);
 
-        Calculate_area_and_perimeter( c );
+        Calculate_area_and_perimeter(c);
 
         lst[cnt++] = c;
     }
@@ -50,12 +48,10 @@ int main()
     printf("Total %d circles\n", cnt);
     fclose(input);
 
-    for (int i = 0; i < cnt; i++)
-    {
+    for (int i = 0; i < cnt; i++) {
         c = lst[i];
 
-        for (int j = i + 1; j < cnt; j++)
-        {
+        for (int j = i + 1; j < cnt; j++) {
             c2 = lst[j];
 
             if (sqr(c->x - c2->x) + sqr(c->y - c2->y) <= abs(c->r - c2->r))
