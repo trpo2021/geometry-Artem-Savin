f39bfef1a3b0f8a333355b08ce0ae69c0cc0cc04
Artem_Savin <artemsavin2305@rambler.ru>
2021-04-03 12:02:17 +0700

    The code is formatted

--- test/test.c
+++ test/test.c
@@ -2,57 +2,61 @@
 #define TESTS_H
 
 #define CTEST_MAIN
-
 #include "ctest.h"
 
 CTEST(suite, test1)
 {
-    ASSERT_EQUAL( 1+2, 3 );
+    ASSERT_EQUAL(1 + 2, 3);
 }
 
-CTEST(suite, test2) {
+CTEST(suite, test2)
+{
     TCircle c;
     char result[1000];
 
-    Correct_Writing( "", &c, result);
+    Correct_Writing("", &c, result);
 
-    ASSERT_NOT_EQUAL( result[0], 0 );
+    ASSERT_NOT_EQUAL(result[0], 0);
 }
 
-CTEST(suite, test3) {
+CTEST(suite, test3)
+{
     TCircle c;
     char result[1000];
 
-    Correct_Writing( "oval(10 10, 5)", &c, result);
+    Correct_Writing("oval(10 10, 5)", &c, result);
 
-    ASSERT_NOT_EQUAL( result[0], 0 );
+    ASSERT_NOT_EQUAL(result[0], 0);
 }
 
-CTEST(suite, test4) {
+CTEST(suite, test4)
+{
     TCircle c;
     char result[1000];
 
-    Correct_Writing( "circle( 10 )", &c, result);
+    Correct_Writing("circle( 10 )", &c, result);
 
-    ASSERT_NOT_EQUAL( result[0], 0 );
+    ASSERT_NOT_EQUAL(result[0], 0);
 }
 
-CTEST(suite, test5) {
+CTEST(suite, test5)
+{
     TCircle c;
     char result[1000];
 
-    Correct_Writing( "circle( 10,10,10 )", &c, result);
+    Correct_Writing("circle( 10,10,10 )", &c, result);
 
-    ASSERT_NOT_EQUAL( result[0], 0 );
+    ASSERT_NOT_EQUAL(result[0], 0);
 }
 
-CTEST(suite, test6) {
+CTEST(suite, test6)
+{
     TCircle c;
     char result[1000];
 
-    Correct_Writing( "circle)", &c, result);
+    Correct_Writing("circle)", &c, result);
 
-    ASSERT_NOT_EQUAL( result[0], 0 );
+    ASSERT_NOT_EQUAL(result[0], 0);
 }
 
 #endif
